b61eb471cd17b6f23ada2361d676cc796fce18e0
ColinKlich <colinklich23@gmail.com>
2022-10-20 08:34:42 -0500

    edit3

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,10 +1,35 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-console.log(process.env.NODE_ENV);
+const DB = process.env.DATABASE;
 
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => {console.log('DB connection successful');
+  });
+  /*
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri =
+  'mongodb+srv://guest1:<3BkeLHu1wsS2LKhx>@cluster0.gcxouic.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  console.log('DB connection successful');
+  const collection = client.db('natours').collection('tours');
+  // perform actions on the collection object
+  client.close();
+});
+*/
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+//mongodb+srv://guest1:<3BkeLHu1wsS2LKhx>@cluster0.gcxouic.mongodb.net/admin
