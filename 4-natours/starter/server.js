1d9dfa18068042efc1074a3eb45815be1396255b
ColinKlich <colinklich23@gmail.com>
2022-11-03 10:10:26 -0500

    edit1.1

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,35 +1,39 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = process.env.DATABASE;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
   })
-  .then(() => {console.log('DB connection successful');
-  });
-  /*
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri =
-  'mongodb+srv://guest1:<3BkeLHu1wsS2LKhx>@cluster0.gcxouic.mongodb.net/?retryWrites=true&w=majority';
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
-client.connect((err) => {
-  console.log('DB connection successful');
-  const collection = client.db('natours').collection('tours');
-  // perform actions on the collection object
-  client.close();
-});
-*/
+  .then(() => console.log('DB connection successful!'));
+
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
 
 //mongodb+srv://guest1:<3BkeLHu1wsS2LKhx>@cluster0.gcxouic.mongodb.net/admin
